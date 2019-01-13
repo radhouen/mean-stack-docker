df977777c63d4364e00a0202365bb999c0791029
assakra radhouen <askriradhouen@gmail.com>
2019-01-13 11:43:48 +0100

    Change HttpClient for angular project

--- angular-app/src/app/app.module.ts
+++ angular-app/src/app/app.module.ts
@@ -5,14 +5,18 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 
 
+import {HttpClientModule} from '@angular/common/http';
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        HttpClientModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
